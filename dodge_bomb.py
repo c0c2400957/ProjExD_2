046b438b1d55c1c73352f2eaa2e0ecb55e6de63b
C0C24009 <c0c2400957@edu.teu.ac.jp>
2025-09-30 14:14:53 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,9 +1,18 @@
 import os
+import random
 import sys
 import pygame as pg
 
+
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,15 +21,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-
-    # 練習問題1: 押下キーと移動量の対応辞書
-    delta = {
-        pg.K_UP:    (0, -5),
-        pg.K_DOWN:  (0, +5),
-        pg.K_LEFT:  (-5, 0),
-        pg.K_RIGHT: (+5, 0),
-    }
-
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空Surface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い爆弾円
+    bb_img.set_colorkey((0, 0, 0))  # 四隅の黒い部分を透過
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
+    vx, vy = +5, +5  # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -31,17 +38,28 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        # 辞書のキー（pg.K_UPなど）と値（(0, -5)など）をループで取り出す
-        for key, mv in delta.items():
-            if key_lst[key]: # key_lstにキーが存在し、その値がTrue（押されている）場合
-                sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
-
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  # 横方向の移動量を加算
+                sum_mv[1] += mv[1]  # 縦方向の移動量を加算
+        # if key_lst[pg.K_w]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  # 爆弾移動
+        screen.blit(bb_img, bb_rct)  # 爆弾描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
 
 if __name__ == "__main__":
     pg.init()
