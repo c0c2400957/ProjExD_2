7dd27197cc1bb5a61f337380e550013547560669
C0C24009 <c0c2400957@edu.teu.ac.jp>
2025-09-30 14:05:33 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,10 +2,8 @@ import os
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -14,6 +12,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    # 練習問題1: 押下キーと移動量の対応辞書
+    delta = {
+        pg.K_UP:    (0, -5),
+        pg.K_DOWN:  (0, +5),
+        pg.K_LEFT:  (-5, 0),
+        pg.K_RIGHT: (+5, 0),
+    }
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -24,20 +31,17 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        # 辞書のキー（pg.K_UPなど）と値（(0, -5)など）をループで取り出す
+        for key, mv in delta.items():
+            if key_lst[key]: # key_lstにキーが存在し、その値がTrue（押されている）場合
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
